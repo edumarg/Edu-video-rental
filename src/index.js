5171511c60631edd49eed99f24b7b556d2505734
Eduardo Margulis <edumarg@gmx.com>
2020-12-16 10:30:49 +0200

    added framework for pagination and side menu

--- src/index.js
+++ src/index.js
@@ -13,9 +13,24 @@ ReactDOM.render(
     <Movies />
     <footer>
       Background image Designed and provide by{" "}
-      <a href="http://www.freepik.com" target="_blank">
+      <a href="http://www.freepik.com" target="_blank" rel="noreferrer">
         starline / Freepik
       </a>
+      <div>
+        Favicon made by{" "}
+        <a
+          href="https://www.flaticon.com/authors/photo3idea-studio"
+          title="photo3idea_studio"
+          target="_blank"
+          rel="noreferrer"
+        >
+          photo3idea_studio
+        </a>{" "}
+        from{" "}
+        <a href="https://www.flaticon.com/" title="Flaticon">
+          www.flaticon.com
+        </a>
+      </div>
     </footer>
   </React.StrictMode>,
   document.getElementById("root")
