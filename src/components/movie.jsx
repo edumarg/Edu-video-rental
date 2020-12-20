3675687ade4862a1a0a7d76df197457579c8c8ee
Eduardo Margulis <edumarg@gmx.com>
2020-12-20 12:38:51 +0200

    added framework for movies and customers pages

--- src/components/movie.jsx
+++ src/components/movie.jsx
@@ -2,10 +2,18 @@ import React, { Component } from "react";
 
 class Movie extends Component {
   state = {};
+
+  handleOnSave() {
+    return;
+  }
+
   render() {
     return (
       <div>
         <h2>Movie information for {}</h2>
+        <button className="btn btn-primary" onClick={() => this.handleOnSave()}>
+          Save
+        </button>
       </div>
     );
   }
