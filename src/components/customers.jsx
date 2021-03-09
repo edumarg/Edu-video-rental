63c7d9cb5eadb2b82d50db89e74226787f36da8c
Eduardo Margulis <edumarg@gmx.com>
2021-03-09 10:49:08 +0200

    built the customers table

--- src/components/customers.jsx
+++ src/components/customers.jsx
@@ -4,30 +4,46 @@ class Customers extends Component {
   state = {};
   render() {
     const { customers } = this.props;
+    const count = customers.length;
     return (
       <React.Fragment>
         <div className="component-div">
           <h1 className="title">Customers</h1>
-          <div className="table-div">
-            <table className="table">
-              <thead>
-                <tr>
-                  <th scope="col">Name</th>
-                  <th scope="col">email</th>
-                </tr>
-              </thead>
-              <tbody className="movies-table">
-                {customers.map((customer) => {
-                  return (
-                    <tr key={customer._id}>
-                      <td>{customer.name}</td>
-                      <td>{customer.email}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
+          {count === 0 && (
+            <div>
+              <h2>No Customers found</h2>
+            </div>
+          )}
+          {count > 0 && (
+            <div className="table-div">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Gold</th>
+                  </tr>
+                </thead>
+                <tbody className="movies-table">
+                  {customers.map((customer) => {
+                    return (
+                      <tr key={customer._id}>
+                        <td>{customer.name}</td>
+                        <td>{customer.phone}</td>
+                        <td>
+                          {customer.isGold && (
+                            <span className="badge rounded-pill bg-warning text-dark py-2 px-3">
+                              Gold
+                            </span>
+                          )}
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          )}
         </div>
       </React.Fragment>
     );
